3271b978439d4231464770e0312f5af90c4125bb
ujjawal-1999 <jainujjawal1999@gmail.com>
2020-11-19 12:35:47 +0530

    Added all admin routes (Needs to be tested)

--- src/config/key.js
+++ src/config/key.js
@@ -1,6 +1,4 @@
 module.exports = {
   MONGODB_URL:
     "mongodb+srv://ujjawal:ujjawal@cluster0.aroce.mongodb.net/testing?retryWrites=true&w=majority",
-  ADMIN_EMAIL: "jainujjawal1999@gmail.com",
-  ADMIN_PASSWORD: "portfolio2020",
 };
--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,24 +1,46 @@
 const mongoose = require("mongoose");
-const { ObjectId } = mongoose.Schema.Types;
+const bcrypt = require("bcryptjs");
 
 const AdminSchema = new mongoose.Schema(
   {
+    username: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
     name: {
       type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      default:
-        "https://cdn-images-1.medium.com/max/800/1*fDv4ftmFy4VkJmMR7VQmEA.png",
     },
     description: {
       type: String,
+    },
+    image: {
+      type: String,
+    },
+    details: [
+      {
+        title: String,
+        description: String,
+      },
+    ],
+    contact: {
+      email: String,
+      phone: String,
+      facebook: String,
+      linkedin: String,
     },
   },
   {
     timestamps: true,
   }
 );
+
+//To hash the password
+AdminSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 module.exports = mongoose.model("Admin", AdminSchema);
