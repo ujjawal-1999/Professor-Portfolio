eec9650bfa58678cfdfb13d65a1151831e664bf0
milibar <milind.barman30@gmail.com>
2020-11-20 12:25:05 +0530

    Forms click functionality added

---
+++ src/js/modal.js
@@ -0,0 +1,8 @@
+function addbooks(){
+    document.getElementById("NewBook").style.opacity="1";
+    document.getElementById("NewBook").style.zIndex="5";
+}
+function editbook(){
+    document.getElementById("EditBook").style.opacity="1";
+    document.getElementById("EditBook").style.zIndex="5";
+}
